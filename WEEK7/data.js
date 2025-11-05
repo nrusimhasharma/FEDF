a63456fad2127e988b91fbcd5000e1ffb04f7461
nrusimhasharma <avadhanulanrusimha@gmail.com>
2025-11-05 08:52:58 +0530

    Add files via upload

---
+++ WEEK7/data.js
@@ -0,0 +1,6 @@
+export const books = [
+  { title: "HP", author: "Mark", price: 4909, availability: "in stock" },
+  { title: "LENOVO", author: "Mask", price: 699, availability: "in stock" },
+  { title: "ASUS", author: " Nolan", price: 59999, availability: "out of stock" },
+  { title: "DELL", author: "Clark", price: 4500, availability: "in stock" }
+];
